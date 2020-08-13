830339cbd90fb2f9c33c258fe676412a06015784
Sigari-Dev <58165188+Sigari-Dev@users.noreply.github.com>
2020-08-13 22:55:22 +0430

    update

---
+++ config.py
@@ -0,0 +1,6 @@
+from pyrobot.sample_config import Config
+
+class Development(Config):
+  APP_ID = 1473647
+  API_HASH = "6ff32a5d80fbe15415ce2abd46c138e2"
+  HU_STRING_SESSION = ""
---
+++ pyroself/__main__.py
@@ -0,0 +1,42 @@
+from pyrogram import Client
+from pyrogram import __version__
+from pyrogram.api.all import layer
+from pyroself.plugins import ALL_PLUGINS
+from pyroself import (
+    APP_ID,
+    API_HASH,
+    HU_STRING_SESSION,
+    LOGGER,
+    load_cmds)
+
+class PyroBot(Client):
+
+    def __init__(self):
+        name = self.__class__.__name__.lower()
+
+        super().__init__(
+            HU_STRING_SESSION,
+            plugins=dict(root=f"{name}/plugins"),
+            workdir=f"{name}/session",
+            api_id=APP_ID,
+            api_hash=API_HASH)
+
+
+    async def start(self):
+        await super().start()
+        result = load_cmds(ALL_PLUGINS)
+        LOGGER.info(result)
+
+        usr_bot_me = await self.get_me()
+        LOGGER.info(
+            f"Pyro-Self based on Pyrogram v{__version__} "
+            f"(Layer {layer}) started..."
+            f"Hey {usr_bot_me.first_name}")
+
+
+    async def stop(self, *args):
+        await super().stop()
+        LOGGER.info("Pyro-Self stopped. Bye.")
+
+if __name__ == "__main__":
+    PyroBot().run()
