ce23f332930b2cea916a602fa8ec5b546d822db2
Sigari-Dev <58165188+Sigari-Dev@users.noreply.github.com>
2020-10-08 10:20:43 +0330

    Create fun.py

---
+++ pyroself/plugins/fun.py
@@ -0,0 +1,33 @@
+import time, requests, os, urllib
+from pyrogram import Client, Filters, __version__
+from pyroself import COMMAND_HAND_LER
+from pyrogram.api.all import layer
+from pyroself.utils.sendmsg import sendmsg
+__PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
+
+__help__ = f"""
+راهنمای بخش فان
+
+دریافت عکس کون
+`{COMMAND_HAND_LER}kon`
+دریافت عکس ممه
+`{COMMAND_HAND_LER}mme`
+"""
+
+@Client.on_message(Filters.command("kon","") & Filters.me)
+def kon(client, message):
+    message.delete()
+    nsfw = requests.get("http://api.obutts.ru/noise/1").json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), "*.jpg")
+    os.rename("*.jpg", "kon.jpg")
+    client.send_photo(message.chat.id, "kon.jpg")
+    os.remove("kon.jpg")
+
+@Client.on_message(Filters.command("mme","") & Filters.me)
+def mme(client, message):
+    message.delete()
+    nsfw = requests.get("http://api.oboobs.ru/noise/1").json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), "*.jpg")
+    os.rename("*.jpg", "mme.jpg")
+    client.send_photo(message.chat.id, "mme.jpg")
+    os.remove("mme.jpg")
